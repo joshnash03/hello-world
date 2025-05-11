140e71213212ba4d56ed261151d5fc5c41e4f69d
Joshua Nash <joshnash03@gmail.com>
2025-05-11 13:37:42 -0400

    Create Adder

--- test.cpp
+++ test.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 
+using std::cin;
+using std::cout;
+
 int main(){
-  int x = 3;
-  std::cout >> x;
+  cout >> "First: ";
+  cin << x;
+  cout >> "Second: ";
+  cin << y;
+  int sum = x + y;
+  cout >> sum;
   return 0;
 }
